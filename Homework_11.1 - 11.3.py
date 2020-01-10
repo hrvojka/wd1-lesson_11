c31e914a2225af4f15d76e1035d06e41c18e48f7
Hrvojka Štrtak <hrvojka.strtak@gmail.com>
2020-01-11 00:00:10 +0100

    corrected

--- Homework_11.1 - 11.3.py
+++ Homework_11.1 - 11.3.py
@@ -13,7 +13,9 @@ with open("score_list.txt", "r") as score_file:
     score_list.sort(key=itemgetter("attempts"))
 
 for score_dict in score_list[0:3]:
-    print(f' NAME: {str(score_dict["name"])}, ATTEMPTS: {str(score_dict["attempts"])}, SECRET NUMBER: {str(score_dict["secret"])}, WRONG GUESSES: {str(score_dict["wrong_guesses"])}, DATE: {score_dict["date"]}')
+    print(f'NAME: {str(score_dict["name"])}, ATTEMPTS: {str(score_dict["attempts"])},'
+          f' SECRET NUMBER: {str(score_dict["secret"])}, WRONG GUESSES: {str(score_dict["wrong_guesses"])},'
+          f' DATE: {score_dict["date"]}')
 
 name = input("Name: ")
 
@@ -22,14 +24,21 @@ while True:
     guess = int(input("Guess: "))
 
     if secret == guess:
-        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses": wrong_guesses, "secret": secret})
+        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()),
+                           "name": name, "wrong_guesses": wrong_guesses, "secret": secret})
+
         with open("score_list.txt", "w") as score_file:
             score_file.write(json.dumps(score_list))
+
         print("You've guessed it - congratulations! It's number " + str(secret))
         print("Attempts needed: " + str(attempts))
         break
+
     elif guess > secret:
         print("Your guess is not correct... try something smaller")
+
     elif guess < secret:
         print("Your guess is not correct... try something bigger")
+
     wrong_guesses.append(guess)
+
